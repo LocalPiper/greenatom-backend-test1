b5d9d74b13eb6f8f306b075ec82bcfa5cada3f8d
Local Piper <artsor53@gmail.com>
2024-11-05 15:14:29 +0300

    feat: preparation for testing

--- project/app/src/database.py
+++ project/app/src/database.py
@@ -1,9 +1,10 @@
 # database.py
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://fastapi:fastapi@db/project"
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://fastapi:fastapi@db:5432/project")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
